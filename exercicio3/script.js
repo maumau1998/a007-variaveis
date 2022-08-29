b5b0a08f74d26b22fa9c5c8c5bdbe85278279d97
maumau1998 <kemillyleite85@gmail.com>
2022-08-29 20:27:30 -0300

    exercicio-003

---
+++ exercicio3/script.js
@@ -0,0 +1,12 @@
+let A = 150;
+let B = 350;
+
+console.log("Valor antigo :")
+console.log("A =", A);
+console.log("B =", B);
+
+let Trocar = A;
+
+console.log("Valor atual :")
+console.log("A =",A=B);
+console.log("B =",B=Trocar);
